0047e08bbc9c5c70b1c84776d3733537eedc524c
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-11-15 13:01:04 +0530

    commit2

--- expense.js
+++ expense.js
@@ -1,110 +1,101 @@
-
-let form=document.getElementById('add');
-form.addEventListener('click', addExpense);
-function addExpense(e)
-{   e.preventDefault();
-    const amount=document.getElementById('amount').value;
-    const description=document.getElementById('description').value;
-    if(amount.trim()==="" || description.trim()==="")
-    {
-        alert("Please Enter Valid Text and Amount");
-    }
-    else
-    {
-    const id=generateId();
-    const category=document.getElementById('categories').value;
-    var obj={
-        Amount:amount,
-        Description:description,
-        Category:category
+let form = document.getElementById("add");
+form.addEventListener("click", addExpense);
+function addExpense(e) {
+  e.preventDefault();
+  const amount = document.getElementById("amount").value;
+  const description = document.getElementById("description").value;
+  if (amount.trim() === "" || description.trim() === "") {
+    alert("Please Enter Valid Text and Amount");
+  } else {
+    const category = document.getElementById("categories").value;
+    var obj = {
+      Amount: amount,
+      Description: description,
+      Category: category,
     };
-
-    // localStorage.setItem(id,JSON.stringify(obj));
-    axios.post("https://crudcrud.com/api/7b3fbbdeecaf454e859152c6b524dad9/booking",obj)
-    .then(function(value){
+    axios
+      .post(
+        "https://crudcrud.com/api/f35fff80a8324aba8c8ddcc310e87368/expense-tracker",
+        obj
+      )
+      .then(function (value) {
         console.log(value);
-    })
-    .catch(function(error){
+      })
+      .catch(function (error) {
         console.log(error);
-    })
-    const component=document.getElementById('uList');
-    const newElement=document.createElement('li');
-    newElement.id=id;
-    newElement.className="fs-5";
-    newElement.innerHTML=`${description}  ${amount}  ${category}  `; 
+      });
+    const component = document.getElementById("uList");
+    const newElement = document.createElement("li");
+    newElement.className = "fs-5";
+    newElement.innerHTML = `${description}  ${amount}  ${category}  `;
     component.appendChild(newElement);
-    const newBtn=document.createElement('button');
-    const editBtn=document.createElement('button');
-    newBtn.className="btn btn-danger";
-    editBtn.className="btn btn-secondary";
-    newBtn.textContent='Del';
-    editBtn.textContent='Edit';
-    newElement.setAttribute("onclick", "remove(this)"); 
+    const newBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+    newBtn.className = "btn btn-danger";
+    editBtn.className = "btn btn-secondary";
+    newBtn.textContent = "Del";
+    editBtn.textContent = "Edit";
+    newBtn.onclick = () => {
+      deleteUser(newElement.id);
+    };
     editBtn.setAttribute("onclick", "edit(this)");
     newElement.appendChild(newBtn);
     newElement.appendChild(editBtn);
-    }
-    clearInputs() ;
- }
-function clearInputs() { 
-    document.getElementById('amount').value = ""; 
-    document.getElementById('description').value = ""; 
-
-} 
-function remove(e)
-{
-    let removeElement=e.id;
-    localStorage.removeItem(removeElement);
-    e.parentNode.removeChild(e);
+  }
+  clearInputs();
 }
-function edit(e)
-{
-    let removeElement=e.parentNode.id;
-    let obj = JSON.parse(localStorage.getItem(removeElement));
-    let amount=obj.Amount;
-    let desc=obj.Description;
-    document.getElementById('amount').value =amount;
-    document.getElementById('description').value =desc;
-    localStorage.removeItem(removeElement);
+function clearInputs() {
+  document.getElementById("amount").value = "";
+  document.getElementById("description").value = "";
 }
-function generateId()
-{
-    return Math.floor(Math.random()*10000);
-}
-window.onload = function() {
-    
-    getSavedData()
-    
-}
-function getSavedData()
-{
-    axios.get("https://crudcrud.com/api/7b3fbbdeecaf454e859152c6b524dad9/booking")
-    .then(function(resp){
-       
-        display(resp.data);
+window.addEventListener("DOMContentLoaded", function () {
+  displaySavedData();
+});
+function deleteUser(id) {
+  axios
+    .delete(
+      `https://crudcrud.com/api/f35fff80a8324aba8c8ddcc310e87368/expense-tracker/${id}`
+    )
+    .then(function () {
+      console.log("deleted");
     })
+    .catch(function (error) {
+      console.log("Something went wrong");
+    });
 }
-function display(arr)
-{
-    for(let i=0;i<arr.length;i++)
-    {
-        const amount=arr[i].Amount;
-        const description=arr[i].Description;
-        const category=arr[i].Category;
-    const component=document.getElementById('uList');
-    const newElement=document.createElement('li');
-    newElement.className="fs-5";
-    newElement.innerHTML=`${description}  ${amount}  ${category}  `; 
-    component.appendChild(newElement);
-    const newBtn=document.createElement('button');
-    const editBtn=document.createElement('button');
-    newBtn.className="btn btn-danger";
-    editBtn.className="btn btn-secondary";
-    newBtn.textContent='Del';
-    editBtn.textContent='Edit';
-    newElement.setAttribute("onclick", "remove(this)"); 
-    editBtn.setAttribute("onclick", "edit(this)");
-    newElement.appendChild(newBtn);
-    newElement.appendChild(editBtn);
-    }
+function displaySavedData() {
+  axios
+    .get(
+      "https://crudcrud.com/api/f35fff80a8324aba8c8ddcc310e87368/expense-tracker"
+    )
+    .then(function (response) {
+      const arr = response.data;
+      for (let i = 0; i < arr.length; i++) {
+        const userId = arr[i]._id;
+        const amount = arr[i].Amount;
+        const description = arr[i].Description;
+        const category = arr[i].Category;
+        const component = document.getElementById("uList");
+        const newElement = document.createElement("li");
+        newElement.className = "fs-5";
+        newElement.innerHTML = `${description}  ${amount} ${category}  `;
+        component.appendChild(newElement);
+        const newBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
+        newBtn.className = "btn btn-danger";
+        editBtn.className = "btn btn-secondary";
+        newBtn.textContent = "Del";
+        editBtn.textContent = "Edit";
+        newElement.id = userId;
+        newBtn.onclick = () => {
+          deleteUser(newElement.id);
+        };
+        editBtn.setAttribute("onclick", "edit(this)");
+        newElement.appendChild(newBtn);
+        newElement.appendChild(editBtn);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
